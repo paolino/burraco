ba6668f241b9ea37ac8b7164333680b3f0cc9f19
paolino <paolino@paolinoub.(none)>
2009-11-01 22:14:27 +0100

    cleanings

--- Burraco.hs
+++ Burraco.hs
@@ -11,27 +11,6 @@ import Enumeration
 import Debug.Trace
 
 newtype Rank = Rank Int deriving (Read,Ord,Eq,Num, Enum)
-unRank (Rank x) = x
-
-
-newtype Suite = Suite Int deriving (Read,Ord,Eq)
-instance Show Suite where
-	show (Suite y) 
-		| y == 0 = "C"
-		| y == 1 = "Q"
-		| y == 2 = "F"
-		| y == 3 = "P"
-unSuite (Suite x) = x
-
-
-newtype Card = Card (Rank, Suite) deriving (Read,Eq, Ord)
-unCard (Card x) = x
-
-instance Show Card where
-	show (Card (x,y)) 
-		| x == -20 = "Jolly"
-		| otherwise = show x ++ show y
-
 instance Show Rank where
 	show (Rank x) 
 		| x == 1 = "A"
@@ -40,12 +19,23 @@ instance Show Rank where
 		| x == 11 = "J"
 		| x == 14 = "A"
 		| otherwise = show $ x
-{- 
-instance Read Card where
-	readsPrec _ "Jolly" = [(Card (-20,-20),"")]
-	readsPrec _ (r:s:"") 
-		| s == "C" = [Card (
--}
+
+newtype Suite = Suite Int deriving (Read,Ord,Eq,Num)
+instance Show Suite where
+	show (Suite y) 
+		| y == 0 = "C"
+		| y == 1 = "Q"
+		| y == 2 = "F"
+		| y == 3 = "P"
+
+
+newtype Card = Card {unCard :: (Rank, Suite)} deriving (Read,Eq, Ord)
+
+instance Show Card where
+	show (Card (x,y)) 
+		| x == -20 = "Jolly"
+		| otherwise = show x ++ show y
+
 type Hand = [Card]
 
 data PSA = Piazzato Rank Bool  | Spiazzato Bool | Assente deriving (Show,Eq,Ord,Read)
@@ -67,14 +57,14 @@ isScarto (Scarto _) = True
 isScarto _ = False
 
 suite =  snd . unCard
-jolly = Card (-20,Suite (-20))
+jolly = Card (-20,-20)
 
 isJP = liftM2 (||) (ranked 2) (ranked (-20))
 
 
 ranked n = (==) n . fst . unCard
 rankedWA n = ranked $ case n of 
-	Rank 14 ->  Rank 1
+	14 -> 1
 	x -> x
 suited s = (==) s . suite
 
@@ -102,27 +92,27 @@ instance CG Game Card where
 		-- attaccare ad una scala con jolly o pinella all'interno
 		attach' (Scala s (Piazzato r t) (r0,r1)) c@(Card (r', s')) h 
 			| s /= s' = nochance 
-			| r == r' && t && r0 == Rank 3 = subst h $ Scala s Assente (Rank 2,r1)-- la pinella va a finire nel 2  
+			| r == r' && t && r0 == 3 = subst h $ Scala s Assente (2,r1)-- la pinella va a finire nel 2  
 			| r == r' = subst h $ Scala s (Spiazzato t) (r0,r1) -- la pinella ha lo stesso seme ma non finisce nel 2
-			| r' == r0 - Rank 1 = subst h $ Scala s (Piazzato r t) (r',r1)
-			| r' == r1 + Rank 1 = subst h $ Scala s (Piazzato r t) (r0,  r')
+			| r' == r0 - 1 = subst h $ Scala s (Piazzato r t) (r',r1)
+			| r' == r1 + 1 = subst h $ Scala s (Piazzato r t) (r0,  r')
 			| otherwise = nochance
 
 		-- attaccare ad una scala con jolly o pinella all'esterno
 		attach' (Scala s (Spiazzato t) (r0,r1)) c@(Card (r', s')) h 
 			| s /= s' = nochance 
-			| r' == r0 - Rank 1 = subst h $ Scala s (Spiazzato t) (r',r1)
-			| r' == r0 - Rank 2 = subst h $ Scala s (Piazzato (r0 - Rank 1) t) (r',r1)
-			| r' == r1 + Rank 1 = subst h $ Scala s (Spiazzato t) (r0, r') 
-			| r' == r1 + Rank 2 = subst h $ Scala s (Piazzato (r0 + Rank 1) t) (r', r1)
+			| r' == r0 - 1 = subst h $ Scala s (Spiazzato t) (r',r1)
+			| r' == r0 - 2 = subst h $ Scala s (Piazzato (r0 - 1) t) (r',r1)
+			| r' == r1 + 1 = subst h $ Scala s (Spiazzato t) (r0, r') 
+			| r' == r1 + 2 = subst h $ Scala s (Piazzato (r0 + 1) t) (r', r1)
 			| otherwise = nochance
 
 		-- attaccare ad una scala che non contiene jolly o pinelle
 		attach' (Scala s Assente (r0,r1))  c@(Card (r', s')) h
 			| s /= s' && isJP c = subst h $ Scala s (Spiazzato False) (r0,r1)
 			| s /= s' = nochance
-			| r' == r0 - Rank 1 = subst h $ Scala s Assente (r',r1)  
-			| r' == r1 + Rank 1 = subst h $ Scala s Assente (r0, r') 
+			| r' == r0 - 1 = subst h $ Scala s Assente (r',r1)  
+			| r' == r1 + 1 = subst h $ Scala s Assente (r0, r') 
 			| otherwise = nochance
 
 		-- attaccare al tavolo ovvero calare una figura nuova
@@ -133,35 +123,35 @@ instance CG Game Card where
 				checks s = zipWith (&.&) (repeat $ suited s) 
 				t = let 	k (Right _) = Tris r True
 						k _ = Tris r False
-						in block k  $ picks (Rank 0) (replicate 2 $ ranked r) (Left isJP, h)
-				s0 r	| r > Rank 2 = let 	
+						in block k  $ picks (0) (replicate 2 $ ranked r) (Left isJP, h)
+				s0 r	| r > 2 = let 	
 						k (Right (n,c)) = let 
 							t = suite  c == s
-							in uncurry (Scala s) $ case n == r - Rank 2 of
-								True -> (Spiazzato t, (r - Rank 1, r))
-								_  -> (Piazzato n t, (r - Rank 2, r))
-						k _ = Scala s Assente (r - Rank 2,r) 
-						in block k $ picks (r - Rank 2) (checks s [ranked (r - Rank 2), ranked (r - Rank 1)]) (Left isJP, h)
+							in uncurry (Scala s) $ case n == r - 2 of
+								True -> (Spiazzato t, (r - 1, r))
+								_  -> (Piazzato n t, (r - 2, r))
+						k _ = Scala s Assente (r - 2,r) 
+						in block k $ picks (r - 2) (checks s [ranked (r - 2), ranked (r - 1)]) (Left isJP, h)
 					| otherwise = nochance
-				s1 r 	| (r >  Rank 1 && r <  Rank 14) = let
-						k (Right  (n,c)) = Scala s (Spiazzato $ suite c == s) $ case n == r - Rank  1 of
-							True -> (r , r +  Rank 1)
-							_ -> (r -  Rank 1, r)
-						k _ = Scala s Assente (r -  Rank 1,r +  Rank 1) 
-						in block k $ picks (r -  Rank 1) (checks s [ ranked (r -  Rank 1), rankedWA (r +  Rank 1)]) (Left isJP, h)
+				s1 r 	| (r >  1 && r <  14) = let
+						k (Right  (n,c)) = Scala s (Spiazzato $ suite c == s) $ case n == r -  1 of
+							True -> (r , r +  1)
+							_ -> (r -  1, r)
+						k _ = Scala s Assente (r -  1,r +  1) 
+						in block k $ picks (r -  1) (checks s [ ranked (r -  1), rankedWA (r +  1)]) (Left isJP, h)
 					| otherwise = nochance
-				s2 r 	| r <  Rank 13 = let
+				s2 r 	| r <  13 = let
 						k (Right (n,c)) = let 
 							t = suite c == s
-							in uncurry (Scala s) $ case n == r +  Rank 2 of
-								True  -> (Spiazzato t, (r, r +  Rank 1))
-								_ -> (Piazzato n t, (r, r +  Rank 2))
-						k _ = Scala s Assente (r, r +  Rank 2) 
-						in block k  $ picks (r +  Rank 1) (checks s [ranked (r +  Rank 1) , rankedWA (r +  Rank 2)])  (Left isJP, h)
+							in uncurry (Scala s) $ case n == r +  2 of
+								True  -> (Spiazzato t, (r, r +  1))
+								_ -> (Piazzato n t, (r, r +  2))
+						k _ = Scala s Assente (r, r +  2) 
+						in block k  $ picks (r +  1) (checks s [ranked (r +  1) , rankedWA (r +  2)])  (Left isJP, h)
 					| otherwise = nochance
 				in concat [t,s0 r ,s1 r ,s2 r] 
 
-type Jolly a = Either (a -> Bool) (Rank, a) -- deriving (Eq,Ord)
+type Jolly a = Either (a -> Bool) (Rank, a) 
 
 pick :: Eq a => ((a -> Bool), Rank) -> (Jolly a,[a]) -> [(Jolly a,[a])]
 pick (k,i) (j,cs) = concat [
