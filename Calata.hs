94081157353b27a33c76a9c23834c00d6b5240c8
paolino <paolino@paolino-desktop.(none)>
2009-11-14 23:04:00 +0100

    Calata reaching the point

--- Calata.hs
+++ Calata.hs
@@ -1,17 +1,41 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 import Card
 import Data.List
 import Data.Ord
 import Control.Arrow
 import Control.Parallel.Strategies
 import Control.Parallel
-import Data.Set hiding (map)
 
+newtype ML = ML [Card] deriving (NFData, Show)
+size (ML x) = length x
+fromList = ML . sort
+toList (ML x) = x
+
+intersection' [] _ xs = xs
+intersection'  _ [] xs = xs
+intersection' (x:xs) (y:ys) zs
+	| y == x = intersection' xs ys (x:zs)
+	| y < x = intersection' (x:xs) ys zs
+	| y > x = intersection' xs ys zs
+
+intersection :: ML -> ML -> ML 
+intersection (ML xs) (ML ys) = fromList $ intersection' xs ys []
+
+difference' [] _ = []
+difference' xs [] = xs
+difference' (x:xs) (y:ys)
+	| x == y = difference' xs ys
+	| x < y = x:difference' xs (y:ys)
+	| x > y = difference' (x:xs) ys
+
+difference :: ML -> ML -> ML
+difference (ML xs) (ML ys) = ML $ difference' xs ys
 -- | compute the subsets of a set, including the empty one (run in exponential space, care)
 powerset []     = [[]]
 powerset (x:xs) = xss ++ map (x:) xss
 	    where xss = powerset xs
 
-type Gioco = Set Card
+type Gioco = ML 
 scale = map fromList $ do
 	j <- [0 .. 3]
 	n <- [1 .. 13]
@@ -24,18 +48,32 @@ trisses = let 	f j = concatMap $ \x -> [x] ++  if j `elem` x then [j:x] else []
 		x<- foldr f (tail . powerset $ [0 .. 3]) [0..3]
 		return . map (Card . (,) n) $ x
 
+
 giochi :: [Gioco]
 giochi = scale ++ trisses  
 
-bests m xs = let 	ws = map (difference xs) giochi
-			ys g = sum $ map (m g) ws
-			byf f g = map g . sortBy (flip $ comparing f) 
-		in byf snd id  . zip giochi . (pseq $ rnf ws) . parMap rnf  ys $ giochi
+type Intersezione = ML
+type Dotazione = ML
 
-simpleEI d g = fromIntegral (size $ intersection d g) / (fromIntegral (size g) ** (0.7 :: Float))
+-- | tabella delle intersezioni per ogni gioco, la lista di intersezioni del gioco stesso con la dotazione privata di tutti i giochi, a turno
+intable :: Dotazione  -> [[Intersezione]]
+intable xs = let 	ws = map (difference xs) giochi -- mani decurtate
+		in   parMap rnf (\g -> map (intersection (intersection g xs)) ws) giochi -- intersezioni di tutti i giochi con le mani decurtate
+
+
+
+type Q =  Gioco -> [Intersezione] -> Float 
+
+classifica :: Q -> [[Intersezione]] -> [(Gioco,Float)]
+classifica q ts = let 
+	by = sortBy (flip $ comparing snd) 
+	in by . zip giochi . parMap rnf  (uncurry q) $ zip giochi ts
+
+simpleEI g = sum . map (\i -> fromIntegral (size i) / (fromIntegral (size g) ** (0.7 :: Float)))
 
 main = do
 	h <- fromList `fmap` runT (handT 11)
 	print h
-	mapM_ print . take 20 $ bests simpleEI  h 
+	let t = intable h
+	mapM_ print  $ classifica simpleEI  t 
 		
